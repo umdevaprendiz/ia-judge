51f73d7e6415b871e788252288b4b964c6842747
Sérgio Souza <187855489+umdevaprendiz@users.noreply.github.com>
2026-09-22 21:26:19 -0300

    Adiciona 2a fase da dosimetria (agravantes e atenuantes)

--- dosimetria/__init__.py
+++ dosimetria/__init__.py
@@ -5,6 +5,8 @@ from .circunstancias import CircunstanciaJudicial, Valoracao
 from .passo import Passo
 from .quantum import EstrategiaQuantum, FracaoDoIntervalo, FracaoDoMinimo
 from .fase1 import ResultadoFase1, calcular_pena_base
+from .agravantes_atenuantes import CircunstanciaLegal, Direcao
+from .fase2 import ResultadoFase2, calcular_pena_intermediaria
 
 __all__ = [
     "Fracao",
@@ -18,4 +20,8 @@ __all__ = [
     "FracaoDoMinimo",
     "ResultadoFase1",
     "calcular_pena_base",
+    "CircunstanciaLegal",
+    "Direcao",
+    "ResultadoFase2",
+    "calcular_pena_intermediaria",
 ]
